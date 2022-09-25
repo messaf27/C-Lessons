1ed282f83d87073aa5eafa54ca8911491d5b738a
Egor <messaf@mail.ru>
2022-09-25 10:21:43 +1000

    Create algoritm add ToLower()

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+string userName = "";
+Console.WriteLine("Enter User Name:");
+userName = Console.ReadLine();
+
+if(userName.ToLower() == "анна"){
+    Console.WriteLine("Привет Анютка!!!");
+}else{
+    Console.WriteLine("Здравствуйте " + userName);
+}
