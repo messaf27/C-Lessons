d6ba2e32bfff76df28565ad50c7563939dd0daf5
Egor <messaf@mail.ru>
2022-09-27 13:05:20 +1000

    add ask #2

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,6 +1,40 @@
-﻿int a = 1;
-int b = 2;
-int c = 6;
-int d = 8;
-int e = 4;
+﻿
+namespace HomeWorks
+{
+    class Program
+    {
+        static public void Task2()
+        {
+            int numOne = 0, numTwo = 0;
 
+            Console.WriteLine("Введите первое число: ");
+            numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите воторое число: ");
+            numTwo = Convert.ToInt32(Console.ReadLine());
+
+            if (numOne > numTwo)
+            {
+                Console.WriteLine($"Первое число {numOne} больше второго {numTwo}");
+            }
+            else if (numOne < numTwo)
+            {
+                Console.WriteLine($"Второе число {numTwo} больше первого {numOne}");
+            }
+            else if (numOne == numTwo)
+            {
+                Console.WriteLine($"Числа равны: ({numOne} == {numTwo})");
+            }
+            else
+            {
+                Console.WriteLine("Вообще не понимаю, что происходит =)");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================= Задача №2 =================");
+            Task2();
+        }
+    }
+}
