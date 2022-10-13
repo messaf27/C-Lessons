70135bb70c9c991661cd98ef7b0d88da1491b874
Egor Orlenok <messaf@mail.ru>
2022-10-14 07:22:45 +1000

    update ask_47 folder

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.WriteLine("=========== Задача №47 ============= ");
+
+double[,] CreateRndArray2D(int m, int n, double rndMin, double rndMax)
+{
+    double[,] array = new int[m, n];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().NextDouble(rndMin, rndMax + 1);    
+        }
+    }
+
+    return array;
+}
+
+void PrintArray2D(double[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}");
+            if(j < (array.GetLength(1) - 1)) Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[,] array2d =  CreateRndArray2D(10, 5, -99.0, 99.0);
+PrintArray2D(array2d);
